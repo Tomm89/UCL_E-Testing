fe75bed4d1e9246a9ede78d09eeb52a31e95a2c8
Tomm89 <brand.tomas89@gmail.com>
2017-01-15 22:10:59 +0100

    Initial Commit

---
+++ UCL_E-Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UCL_E-Testing
+//
+//  Created by tom on 15.01.17.
+//  Copyright © 2017 tom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
